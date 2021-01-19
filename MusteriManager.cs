dd43b9f918f67035fb2ffce20da6ad01cff93a1f
butgizem <55791965+butgizem@users.noreply.github.com>
2021-01-19 15:20:41 +0300

    Create MusteriManager.cs

---
+++ MusteriManager.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.SoyAd);
+        }
+        public void Liste(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.SoyAd);
+        }
+
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.SoyAd);      
+        }
+
+    }
+}
